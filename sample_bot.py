cc56ebb4583f4773e4277e28fa6b162939d677bd
Radu Nicolae <ranicolae@gmail.com>
2015-09-08 22:29:41 -0400

    sample passes pyflakes now

--- sample_bot.py
+++ sample_bot.py
@@ -6,13 +6,13 @@
 import KibotoBot
 
 config = {
-	kiboto_server_hostname: "localhost",
-	kiboto_server_port: 9090,
-	bot_hostname: "localhost",
-	bot_port: 9091,
-	game_id: "sample_game",
-	session_id: "1",
-	player_id: "P1"
+	"kiboto_server_hostname": "localhost",
+	"kiboto_server_port": 9090,
+	"bot_hostname": "localhost",
+	"bot_port": 9091,
+	"game_id": "sample_game",
+	"session_id": "1",
+	"player_id": "P1"
 }
 
 def bot_brain(event_data, player_id, game_id):
