b47c63ba1b7a9a680e8ef99fb9bf8057c92a4307
Radu Nicolae <ranicolae@gmail.com>
2015-09-13 23:27:43 -0400

    updated sample doc comment. passage that no longer applies

--- sample_bot.py
+++ sample_bot.py
@@ -15,8 +15,6 @@ def bot_brain(event_data, player_id, game_id):
 # if you want to run a bot locally and don't care about hostnames or ports,
 # you can initialize like so instead:
 #   mybot = bot.Bot(logic_method=bot_brain, game_id="yourgamehere")
-# or if using the sample game, only supply the logic method:
-#   mybot = bot.Bot(logic_method=bot_brain)
 mybot = kibotobot.Bot(kiboto_server_hostname="http://localhost",
 		kiboto_server_port=9090,
 		bot_hostname="http://localhost",
